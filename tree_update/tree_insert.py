c8b2f0699831a127567e7835112fa22ccf21e7c8
Niels Hanson <nielsh@mail.ubc.ca>
2014-03-04 08:43:26 -0800

    Got tree_insert.py to add multiple children. Works on small example.

--- tree_update/tree_insert.py
+++ tree_update/tree_insert.py
@@ -16,13 +16,13 @@ what_i_do = "inserts a list of organisms into the NCBI Newick format"
 
 parser = argparse.ArgumentParser(description=what_i_do)
 parser.add_argument('--tree', dest='tree', type=str, nargs=1,
-                required=False, help='the input tree in newick format', default=None)
+                required=True, help='the input tree in newick format', default=None)
 parser.add_argument('--id_list', dest='id_list', type=str, nargs=1,
                 required=False, help='list of translation ids for NCBI names', default=None)                
 parser.add_argument('--list', dest='list', type=str, nargs=1,
-               required=False, help='a list of parent child relations by id', default=None)
+               required=True, help='a list of parent child relations by id', default=None)
 parser.add_argument('--out', dest='out', type=str, nargs=1,
-               required=False, help='output names', default=None)
+               required=True, help='output file name of updated tree', default=None)
 
 def fnd(fname, s, start=0):
     with open(fname, 'rb') as f:
@@ -52,30 +52,34 @@ def main():
   id_to_name = {}
   name_to_id = {}
   
-  print "Loading ID Map:"
-  id_handle = open(args['id_list'][0], 'r')
-  id_lines = id_handle.readlines()
-  id_handle.close()
+  # print "Loading ID Map:"
+  # id_handle = open(args['id_list'][0], 'r')
+  # id_lines = id_handle.readlines()
+  # id_handle.close()
+  # 
+  # for l in id_lines:
+  #     fields = l.split("\t")
+  #     name_to_id [ fields[1].strip() ] = fields[0].strip()
+  #     id_to_name [ fields[0].strip() ] = fields[1].strip()
+  # print "Done."
   
-  for l in id_lines:
-      fields = l.split("\t")
-      name_to_id [ fields[1].strip() ] = fields[0].strip()
-      id_to_name [ fields[0].strip() ] = fields[1].strip()
-  print "Done."
-  
-  
-  print "Loading List:"
+  print "Loading New Children List:"
   list_handle = open(args['list'][0], 'r')
-  parent_to_new = {}
+  parent_to_new = {} # parent to list of children to add
+  positions = [] # list of parents to add children to
+  
+  # create the list of children to add
   for l in list_handle:
       fields = l.split("\t")
-      parent_to_new[fields[0].strip()] = fields[1].strip()
-  
+      parent = fields[0].strip()
+      child = fields[1].strip()
+      if parent not in parent_to_new:
+          # create child list
+          parent_to_new[parent] = []
+          positions.append(parent)
+      parent_to_new[parent].append(child)
   list_handle.close()
   print "Done."
-  positions = []
-  for key, value in parent_to_new.iteritems() :
-     positions.append(key)
   
   print "Opening Tree:"
   tree_buff = open(args['tree'][0], "rb")
@@ -86,7 +90,7 @@ def main():
   char = None
   tree_buff.seek(0, os.SEEK_END)
   length = tree_buff.tell()
-  
+  print "  - scanning and adding new children to tree"
   temp_out = open("rev", "w")
   for i in range(length, 0, -1):
       tree_buff.seek(i-1,0)
@@ -106,11 +110,16 @@ def main():
               if num in positions:
                   if char == "(" or char == ",":
                       # no children: print to the left with parenthesis
-                      l = char + "(" + str(parent_to_new[num]) + ")" + num
+                      l = char + "(" + ",".join(parent_to_new[num]) + ")" + num
+                      # print l
                       temp_out.write(l[::-1])
                   elif char == ")":
                       # children: place into children
-                      l = "," + str(parent_to_new[num]) + char + num
+                      l = ""
+                      for c in parent_to_new[num]:
+                          l = l + "," + str(c)
+                      l = l + char + num
+                      # print l
                       temp_out.write(l[::-1])
                   num = ""
                   num_flag = False
@@ -126,17 +135,20 @@ def main():
               num_flag = False
   
   # TODO: test this corner case
-  if num in positions:
-      print num
+  # if num in positions:
+  #     print num
   
   tree_buff.close()
   temp_out.close()
   print "Done."
   
+  # write out reverse tree to disk as rev and then
+  # reverse into output file for correct order.
+  print "Writing out new tree."
   rev_handle = open("rev", "rb")
-  out_handle = open("out", "w")
+  out_handle = open(args['out'][0], "w")
   
-  rev_handle.seek(0, os.SEEK_END)
+  rev_handle.seek(0, os.SEEK_END) # get position of end of file
   length = rev_handle.tell()
   for i in range(length, 0, -1):
       rev_handle.seek(i-1,0)
@@ -147,6 +159,7 @@ def main():
   
   # remove the reversed file
   os.remove("rev")
+  print "Done."
   
   exit()
 
